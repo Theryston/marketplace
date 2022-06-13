cc110cf8aec87a7555157aa182c4bb4dd746f9e7
Theryston <funktodo2@gmail.com>
2022-06-13 15:13:21 -0300

    feat: map tables

---
+++ Marketplace.Infra.Data/Context/MarketplaceDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Marketplace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.Infra.Data.Context
+{
+    internal class MarketplaceDbContext : DbContext
+    {
+        public MarketplaceDbContext(DbContextOptions<MarketplaceDbContext> options) : base(options)
+        { }
+
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MarketplaceDbContext).Assembly);
+        }
+    }
+}
---
+++ Marketplace.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,39 @@
+﻿using Marketplace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Marketplace.Infra.Data.Maps
+{
+    internal class PersonMap : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("person");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasColumnName("id")
+                .UseIdentityColumn();
+
+            builder.Property(c => c.Name)
+                .HasColumnName("name")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Document)
+                .HasColumnName("document")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(c => c.Phone)
+                .HasColumnName("phone")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.HasMany(c => c.Purchases)
+                .WithOne(p => p.Person)
+                .HasForeignKey(c => c.PersonId);
+        }
+    }
+}
